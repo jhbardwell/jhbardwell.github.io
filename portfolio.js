6f93118eb73f50cd4b14812fe2d228a7547c553e
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-03-18 09:45:41 -0400

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -4,39 +4,100 @@ let project1Image = document.getElementById("Newsletter-Concatenation-Program-im
 let project2Image = document.getElementById("Research-Data-Manager-Program-image")
 let project3Image = document.getElementById("Fantasy-Narrative-Program-image")
 
-let project1SystemContent = document.getElementById("GroupName1Div")
-let project1TimelineContent = document.getElementById("GroupName2Div")
-let project1DemoContent = document.getElementById("GroupName3Div")
-let project2SystemContent = document.getElementById("GroupName4Div")
-let project2TimelineContent = document.getElementById("GroupName5Div")
-let project2DemoContent = document.getElementById("GroupName6Div")
-let project3SystemContent = document.getElementById("GroupName7Div")
-let project3TimelineContent = document.getElementById("GroupName8Div")
-let project3DemoContent = document.getElementById("GroupName9Div")
+let project1SystemContent = document.getElementById("RadioButton1-div")
+let project1TimelineContent = document.getElementById("RadioButton2-div")
+let project1DemoContent = document.getElementById("RadioButton3-div")
+let project2SystemContent = document.getElementById("RadioButton4-div")
+let project2TimelineContent = document.getElementById("RadioButton5-div")
+let project2DemoContent = document.getElementById("RadioButton6-div")
+let project3SystemContent = document.getElementById("RadioButton7-div")
+let project3TimelineContent = document.getElementById("RadioButton8-div")
+let project3DemoContent = document.getElementById("RadioButton9-div")
 
-let project1Aside = document.getElementById("Project1Div")
-let project2Aside = document.getElementById("Project2Div")
-let project3Aside = document.getElementById("Project3Div")
+let project1Aside = document.getElementById("Project1-div")
+let project2Aside = document.getElementById("Project2-div")
+let project3Aside = document.getElementById("Project3-div")
 
-function ShowRadioButtonDiv (IdBaseName, NumberOfButtons) {
-    for (x=1;x<=NumberOfButtons;x++) {
-        CheckThisButton = IdBaseName + x;
-        ThisDiv = IdBaseName + x +'Div';
-    if (document.getElementById(CheckThisButton).checked) {
-        document.getElementById(ThisDiv).style.display = "block";
-        }
-    else {
-        document.getElementById(ThisDiv).style.display = "none";
-        }
-    }
-    return false;
+let RadioButtons = document.getElementsByClassName("radiobuttons");
+
+let RadioButton1 = document.getElementById("RadioButton1-input")
+let RadioButton2 = document.getElementById("RadioButton2-input")
+let RadioButton3 = document.getElementById("RadioButton3-input")
+let RadioButton4 = document.getElementById("RadioButton4-input")
+let RadioButton5 = document.getElementById("RadioButton5-input")
+let RadioButton6 = document.getElementById("RadioButton6-input")
+let RadioButton7 = document.getElementById("RadioButton7-input")
+let RadioButton8 = document.getElementById("RadioButton8-input")
+let RadioButton9 = document.getElementById("RadioButton9-input")
+
+let resetAll = function(){
+    document.getElementsByClassName("projectimages").style.filter="sepia(1)";
+    document.getElementsByClassName("contentdivs").style.display="none";
+    document.getElementsByClassName("asidedivs").style.display="none";
 }
 
-function ShowAndHide() {
-    var x = document.getElementById('SectionName');
-    if (x.style.display == 'none') {
-        x.style.display = 'block';
-    } else {
-        x.style.display = 'none';
-    }
+let clickRadioButton1 = function() {
+    project1Image.style.filter="sepia(0)";
+    project1SystemContent.style.display="block";
+    project1Aside.style.display="block";
 }
+
+let clickRadioButton2 = function() {
+    project1Image.style.filter="sepia(0)";
+    project1TimelineContent.style.display="block";
+    project1Aside.style.display="block";
+}
+
+let clickRadioButton3 = function() {
+    project1Image.style.filter="sepia(0)";
+    project1DemoContent.style.display="block";
+    project1Aside.style.display="block";
+}
+
+let clickRadioButton4 = function() {
+    project2Image.style.filter="sepia(0)";
+    project2SystemContent.style.display="block";
+    project2Aside.style.display="block";
+}
+
+let clickRadioButton5 = function() {
+    project2Image.style.filter="sepia(0)";
+    project2TimelineContent.style.display="block";
+    project2Aside.style.display="block"; 
+}
+
+let clickRadioButton6 = function() {
+    project2Image.style.filter="sepia(0)";
+    project2DemoContent.style.display="block";
+    project2Aside.style.display="block";
+}
+
+let clickRadioButton7 = function() {
+    project3Image.style.filter="sepia(0)";
+    project3SystemContent.style.display="block";
+    project3Aside.style.display="block";
+}
+
+let clickRadioButton8 = function() {
+    project3Image.style.filter="sepia(0)";
+    project3TimelineContent.style.display="block";
+    project3Aside.style.display="block";
+}
+
+let clickRadioButton9 = function() {
+    project3Image.style.filter="sepia(0)";
+    project3DemoContent.style.display="block";
+    project3Aside.style.display="block";
+}
+
+RadioButton1.addEventListener("click", clickRadioButton1);
+RadioButton2.addEventListener("click", clickRadioButton2);
+RadioButton3.addEventListener("click", clickRadioButton3);
+RadioButton4.addEventListener("click", clickRadioButton4);
+RadioButton5.addEventListener("click", clickRadioButton5);
+RadioButton6.addEventListener("click", clickRadioButton6);
+RadioButton7.addEventListener("click", clickRadioButton7);
+RadioButton8.addEventListener("click", clickRadioButton8)
+RadioButton9.addEventListener("click", clickRadioButton9);
+
+RadioButtons.addEventListener("blur", resetAll);
