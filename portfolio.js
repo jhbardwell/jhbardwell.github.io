a58e737bf9262399036e1eb7e35d24f4c480f984
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-03-18 21:02:30 -0400

    Add files via upload

--- portfolio.js
+++ portfolio.js
@@ -37,70 +37,66 @@ let resetAll = function(){
 }
 
 let clickRadioButton1 = function() {
-    resetAll();
-    project1Image.style.filter="sepia(0)";
+    project1Image.style.filter="none";
     project1SystemContent.style.display="block";
     project1Aside.style.display="block";
 }
 
 let clickRadioButton2 = function() {
-    project1Image.style.filter="sepia(0)";
+    project1Image.style.filter="none";
     project1TimelineContent.style.display="block";
     project1Aside.style.display="block";
 }
 
 let clickRadioButton3 = function() {
-    project1Image.style.filter="sepia(0)";
+    project1Image.style.filter="none";
     project1DemoContent.style.display="block";
     project1Aside.style.display="block";
 }
 
 let clickRadioButton4 = function() {
-    resetAll();
-    project2Image.style.filter="sepia(0)";
+    project2Image.style.filter="none";
     project2SystemContent.style.display="block";
     project2Aside.style.display="block";
 }
 
 let clickRadioButton5 = function() {
-    project2Image.style.filter="sepia(0)";
+    project2Image.style.filter="none";
     project2TimelineContent.style.display="block";
     project2Aside.style.display="block"; 
 }
 
 let clickRadioButton6 = function() {
-    resetAll();
-    project2Image.style.filter="sepia(0)";
+    project2Image.style.filter="none";
     project2DemoContent.style.display="block";
     project2Aside.style.display="block";
 }
 
 let clickRadioButton7 = function() {
-    project3Image.style.filter="sepia(0)";
+    project3Image.style.filter="none";
     project3SystemContent.style.display="block";
     project3Aside.style.display="block";
 }
 
 let clickRadioButton8 = function() {
-    project3Image.style.filter="sepia(0)";
+    project3Image.style.filter="none";
     project3TimelineContent.style.display="block";
     project3Aside.style.display="block";
 }
 
 let clickRadioButton9 = function() {
-    project3Image.style.filter="sepia(0)";
+    project3Image.style.filter="none";
     project3DemoContent.style.display="block";
     project3Aside.style.display="block";
 }
 
-RadioButton1.addEventListener("click", clickRadioButton1);
-RadioButton2.addEventListener("click", clickRadioButton2);
-RadioButton3.addEventListener("click", clickRadioButton3);
-RadioButton4.addEventListener("click", clickRadioButton4);
-RadioButton5.addEventListener("click", clickRadioButton5);
-RadioButton6.addEventListener("click", clickRadioButton6);
-RadioButton7.addEventListener("click", clickRadioButton7);
-RadioButton8.addEventListener("click", clickRadioButton8)
-RadioButton9.addEventListener("click", clickRadioButton9);
+let clickRadioButtons = function(){
+    for(x=1; x < RadioButtons.length; X++){
+        if (RadioButtons[i].checked){
+            resetAll();
+            clickRadioButton`${i}`();
+        }
+    }
+}
 
-RadioButtons.addEventListener("blur", resetAll);
+RadioButtons.addEventListener("click", clickRadioButtons);
