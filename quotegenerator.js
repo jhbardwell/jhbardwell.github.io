565237a517b13a74b44dfe816e36ffa28f44e3f2
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-04-07 07:47:36 -0400

    Delete quotegenerator.js

--- quotegenerator.js
+++
@@ -1,6 +0,0 @@
-let quotes = quoteStars.concat(quoteImagination,quotePhilosophy,quoteSystems,quoteVocation,quoteWriting);
-
-let paragraph = document.getElementById("Quote-paragraph");
-
-paragraph.innerHTML = quotes[Math.floor(Math.random() * quotes.length)];
-
