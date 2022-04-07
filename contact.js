b463b8bc9ec1c685c96291b2d33e3ad379de22b5
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-04-07 07:36:28 -0400

    Add files via upload

--- contact.js
+++ contact.js
@@ -1,72 +1,65 @@
-let form = document.getElementById("Contact-form")
-let contactLabels = document.getElementsByClassName("contactlabels")
-const submitButton = document.getElementById('Submit-input')
+const form = document.getElementById("Contact-form");
+let submit = document.getElementById("Submit-input");
 
-const names = document.getElementById("Name-input")
-const namesRegex = /^[A-Za-z]+((\s)?((\'|\-|\.)?([A-Za-z])+))*$/
-const namesLabel = document.getElementById('labelForName-input')
-const namesValid = document.getElementById("Name-valid")
+const nameInput = document.getElementById("Name-input");
+const websiteInput = document.getElementById("Website-input");
+const emailInput = document.getElementById("Email-input");
+const messageInput = document.getElementById("Message-input");
 
-const website = document.getElementById("Website-input")
-const websiteRegex = /^((https?|http|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/
-const websiteValid = document.getElementById("Website-valid")
+const nameRegex = /\b[A-Z]*[a-z]*[aeiou]+.+[?^ ][A-Z].{1,19}|\b[A-Z]*[aeiou]+.+[?^,][A-Z].{1,19}/;
+const websiteRegex = /^((https?|http|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/;
+const emailRegex = /^[A-Za-z0-9.!#$%&'*+-/=?^_`{|}~]+@[A-za-z0-9.\-]+[.][A-za-z0-9.\-]+$/;
+const messageRegex = /[A-Za-z]/
+//https://regex101.com/r/7DdyM1/1
 
-const phone = document.getElementById("Phone-input")
-const phoneRegex = /(?:(?:\(?(?:00|\+)([1-4]\d\d|[1-9]\d?)\)?)?[\-\.\ \\\\\/]?)?((?:\(?\d{1,}\)?[\-\.\ \\\\\/]?){0,})(?:[\-\.\ \\\\\/]?(?:#|ext\.?|extension|x)[\-\.\ \\\\\/]?(\d+))?/i
-const phoneValid = document.getElementById("Phone-valid")
+const vowelsRegex = /^.*[A-za-z]*[aeiou]*$/gm;
+const capitallettersRegex = /^.*[A-Z]*$/gm;
+const firstlastnameRegex = /^.*[]*$/gm;
+const completesentencesRegex = /^.*[A-Z][A-Za-z ,:;]+[.!?]*$/gm;
 
-const email = document.getElementById("Email-input")
-const emailRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/
-const emailValid = document.getElementById("Email-valid")
+const nameOutput = document.getElementById("Name-output");
+const websiteOutput = document.getElementById("Website-output");
+const emailOutput = document.getElementById("Email-output");
+const messageOutput = document.getElementById("Message-output");
 
-const skillset = document.getElementsByClassName("skillset")
-const skillsetValid = document.getElementById("Skillset-valid")
+let nameValid = false;
+let websiteValid = false;
+let emailValid = false;
+let messageValid = false;
 
-const programs = document.getElementsByClassName("programs")
-const programsValid = document.getElementById("Programs-valid")
-
-const languages = document.getElementsByClassName("languages")
-const languagesValid = document.getElementById("Languages-valid")
-
-const message = document.getElementById("Message-input")
-const messageRegex = /^\\s+[A-Za-z,;'\"\\s]+[.?!]$/
-const messagesValid = document.getElementById("Messages-valid")
-
-let namesValidationCheck = function (names, namesRegex){
-  if ((names != "") && (namesRegex.exec(names) == true)){
-    namesValid.innerText = "This is a valid name";
-    namesValid.innerText.style.color = "green";
-    return true;
+function inputValidation(input, regex, output) {
+  if ((input.value!="")&&(regex.exec(input.value))){
+        output.innerHTML = `&#x2705;Thanks, your ${input.name} is valid`;
   }
-  else if ((names != "") && (namesRegex.exec(names) == false)) {
-    namesValid.innerText = "This is NOT a valid name";
-    namesValid.innerText.style.color = "red";
-    namesLabel.style.color = "red";
-    return false;
-  }
+  else if ((input.value!="")&&(!regex.exec(input.value))) {
+        output.innerHTML = `&#10060;Sorry, your ${input.name} is <b>not</b> valid`;
+    } 
   else
-    return false;
+      output.textContent="";
 }
 
-let resetAllContacts = function () {
-  for (let i = 0; i < contactLabels.length; i++) {
-    contactLabels[i].style.filter = "gray"; 
+form.addEventListener("input", function(event){
+    switch(event.target.id){
+      case "Name-input":
+        inputValidation(nameInput, nameRegex, nameOutput, nameValid);
+        break;
+      case "Website-input":
+        inputValidation(websiteInput, websiteRegex, websiteOutput, websiteValid);
+        break;
+      case "Email-input":
+        inputValidation(emailInput, emailRegex, emailOutput, emailValid);
+        break;
+      case "Message-input":
+        inputValidation(messageInput, messageRegex, messageOutput, messageValid);
+        break;
+    }
+});
+//make this another switch function looking at output.InnerHTML?
+if(nameValid == true && 
+  websiteValid == true &&
+  emailValid == true &&
+  messageValid == true){
+    submit.disabled == false;
   }
-}
-
-let submitButtonActivation = function(){
-  if(namesValidationCheck()==true){
-    submitButton.disabled=false;
-  }
-}
-
-form.addEventListener("input", function (event) {
-  resetAllContacts();
-  switch (event.target.id){
-    case("Name-input"):
-      namesValidationCheck();
-      break;
-  };
-  submitButtonActivation();
-
-})
+else
+  submit.disabled == true;
