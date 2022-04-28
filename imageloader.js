f236efd372d7bed09b4376f7d09d9cf469e1ea25
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-04-27 23:07:19 -0400

    Add files via upload

---
+++ imageloader.js
@@ -0,0 +1,17 @@
+let imageList = [
+    "https://imgs.xkcd.com/comics/purity.png",
+    "https://sketchplanations.com/the-curb-cut-effect",
+    "https://raw.githubusercontent.com/jhbardwell/jhbardwell.github.io/main/images/projectlimits.png",
+    "https://raw.githubusercontent.com/jhbardwell/jhbardwell.github.io/main/images/header.png",
+    "https://raw.githubusercontent.com/jhbardwell/Narrative-Exploration-Program/main/images/header.png",
+    "https://raw.githubusercontent.com/jhbardwell/Newsletter-Concatenation-Program/main/images/header.png",
+    "https://raw.githubusercontent.com/jhbardwell/Research-Data-Manager-Program/main/images/header.png",
+    ];
+
+let preloadImages = function () {
+    for(let i = 0; i < imageList.length; i++ ) {
+        let imageObject = new Image();
+        imageObject.src = imageList[i];
+    }
+}
+preloadImages();
