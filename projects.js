6d207a97b6005e55efd188c71d9fcf20edb07902
jhbardwell <61172970+jhbardwell@users.noreply.github.com>
2022-04-29 06:35:28 -0400

    Add files via upload

--- projects.js
+++ projects.js
@@ -9,76 +9,93 @@ let portfoliowebpageDiv = document.getElementById("portfoliowebpage-div")
 
 let projectImages = document.getElementsByClassName("projectimages")
 
-let busybeesImage = document.getElementById("Newsletter-Concatenation-Program-image")
-let project2Image = document.getElementById("Research-Data-Manager-Program-image")
+let busybeesImage = document.getElementById("busybees-image")
+let journaltojsonImage = document.getElementById("journaltojson-image")
 let project3Image = document.getElementById("Narrative-Exploration-Program-image")
 let project4Image = document.getElementById("Portfolio-Webpage-image")
 let project5Image = document.getElementById("Kanban-Scriptorium-image")
 
 
+let projectParagraphs = document.getElementsByClassName("projectparagraphs")
+
+let busybeesParagraph = document.getElementById("busybees-paragraph")
+let journaltojsonParagraph = document.getElementById("journaltojson-paragraph")
+let fiveminutefantasyParagraph = document.getElementById("fiveminutefantasy-paragraph")
+let portfoliowebpageParagraph = document.getElementById("portfoliowebpage-paragraph")
+let projkanbanscriptoriumParagraph = document.getElementById("kanbanscriptorium-paragraph")
+
+
 let projectLabels = document.getElementsByClassName("projectlabels")
 
-let busybeesReadMeLabel = document.getElementById("RadioButton1-label") 
-let project2ReadMeLabel = document.getElementById("RadioButton6-label") 
+let busybeesReadMeLabel = document.getElementById("busybees-readme-label") 
+let journaltojsonReadMeLabel = document.getElementById("journaltojson-readme-label") 
 let project3ReadMeLabel = document.getElementById("RadioButton11-label") 
 let project4ReadMeLabel = document.getElementById("RadioButton16-label")
 let project5ReadMeLabel = document.getElementById("RadioButton21-label") 
 
-let busybeesDesignDocLabel = document.getElementById("RadioButton2-label")
-let project2DesignDocLabel = document.getElementById("RadioButton7-label")
+let busybeesDesignDocLabel = document.getElementById("busybees-designdoc-label")
+let pjournaltojsonDesignDocLabel = document.getElementById("journaltojson-designdoc-label")
 let project3DesignDocLabel = document.getElementById("RadioButton12-label")
 let project4DesignDocLabel = document.getElementById("RadioButton17-label")
 let project5DesignDocLabel = document.getElementById("RadioButton22-label")
 
-let busybeesKanbanLabel = document.getElementById("RadioButton3-label")
-let project2KanbanLabel = document.getElementById("RadioButton8-label")
+let busybeesCodeLabel = document.getElementById("busybees-code-label")
+let journaltojsonCodeLabel = document.getElementById("journaltojson-code-label")
+let fiveminutefantasyCodeLabel = document.getElementById("fiveminutefantasy-code-label")
+
+let busybeesKanbanLabel = document.getElementById("busybees-kanban-label")
+let journaltojsonKanbanLabel = document.getElementById("journaltojson-kanban-label")
 let project3KanbanLabel = document.getElementById("RadioButton13-label")
 let project4KanbanLabel = document.getElementById("RadioButton18-label")
 let project5KanbanLabel = document.getElementById("RadioButton23-label")
 
-let busybeesDemoLabel = document.getElementById("RadioButton4-label")
-let project2DemoLabel = document.getElementById("RadioButton9-label")
+let busybeesDemoLabel = document.getElementById("busybees-demo-label")
+let journaltojsonDemoLabel = document.getElementById("journaltojson-demo-label")
 let project3DemoLabel = document.getElementById("RadioButton14-label")
 let project4DemoLabel = document.getElementById("RadioButton19-label")
 let project5DemoLabel = document.getElementById("RadioButton24-label")
 
-let busybeesProgressLabel = document.getElementById("RadioButton5-label")
-let project2ProgressLabel = document.getElementById("RadioButton10-label")
-let project3ProgressLabel = document.getElementById("RadioButton15-label")
-let project4ProgressLabel = document.getElementById("RadioButton20-label")
-let project5ProgressLabel = document.getElementById("RadioButton25-label")
+let busybeesReportLabel = document.getElementById("busybees-report-label")
+let journaltojsonReportLabel = document.getElementById("journaltojson-report-label")
+let project3ReportLabel = document.getElementById("RadioButton15-label")
+let project4ReportLabel = document.getElementById("RadioButton20-label")
+let project5ReportLabel = document.getElementById("RadioButton25-label")
 
-let projectContents = document.getElementsByClassName("contentdivs")
+let projectContents = document.getElementsByClassName("projectcontents")
 
-let busybeesReadMeContent = document.getElementById("RadioButton1-div")
-let project2ReadMeContent = document.getElementById("RadioButton6-div")
+let busybeesReadMeContent = document.getElementById("busybees-readme-div")
+let journaltojsonReadMeContent = document.getElementById("journaltojson-readme-div")
 let project3ReadMeContent = document.getElementById("RadioButton11-div")
 let project4ReadMeContent = document.getElementById("RadioButton16-div")
 let project5ReadMeContent = document.getElementById("RadioButton21-div")
 
-let busybeesDesignDocContent = document.getElementById("RadioButton2-div")
-let project2DesignDocContent = document.getElementById("RadioButton7-div")
+let busybeesDesignDocContent = document.getElementById("busybees-designdoc-div")
+let journaltojsonDesignDocContent = document.getElementById("journaltojson-designdoc-div")
 let project3DesignDocContent = document.getElementById("RadioButton12-div")
 let project4DesignDocContent = document.getElementById("RadioButton17-div")
 let project5DesignDocContent = document.getElementById("RadioButton22-div")
 
-let busybeesKanbanContent = document.getElementById("RadioButton3-div")
-let project2KanbanContent = document.getElementById("RadioButton8-div")
+let busybeesCodeContent = document.getElementById("busybees-code-div")
+let journaltojsonCodeContent = document.getElementById("journaltojson-code-div")
+let fiveminutefantasyCodeContent = document.getElementById("fiveminutefantasy-code-div")
+
+let busybeesKanbanContent = document.getElementById("busybees-kanban-div")
+let journaltojsonKanbanContent = document.getElementById("journaltojson-kanban-div")
 let project3KanbanContent = document.getElementById("RadioButton13-div")
 let project4KanbanContent = document.getElementById("RadioButton18-div")
 let project5KanbanContent = document.getElementById("RadioButton23-div")
 
-let busybeesDemoContent = document.getElementById("RadioButton4-div")
-let project2DemoContent = document.getElementById("RadioButton9-div")
+let busybeesDemoContent = document.getElementById("busybees-demo-div")
+let journaltojsonDemoContent = document.getElementById("journaltojson-demo-div")
 let project3DemoContent = document.getElementById("RadioButton14-div")
 let project4DemoContent = document.getElementById("RadioButton19-div")
 let project5DemoContent = document.getElementById("RadioButton24-div")
 
-let busybeesProgressContent = document.getElementById("RadioButton5-div")
-let project2ProgressContent = document.getElementById("RadioButton10-div")
-let project3ProgressContent = document.getElementById("RadioButton15-div")
-let project4ProgressContent = document.getElementById("RadioButton20-div")
-let project5ProgressContent = document.getElementById("RadioButton25-div")
+let busybeesReportContent = document.getElementById("busybees-report-div")
+let journaltojsonReportContent = document.getElementById("journaltojson-report-div")
+let project3ReportContent = document.getElementById("RadioButton15-div")
+let project4ReportContent = document.getElementById("RadioButton20-div")
+let project5ReportContent = document.getElementById("RadioButton25-div")
 
 let radiobuttons = document.getElementsByClassName("radiobuttons")
 
@@ -86,9 +103,11 @@ let resetAllProjects = function () {
   for (let i = 0; i < projectDivs.length; i++) {
     projectDivs[i].style.order = 1; 
   }
-  
   for (let i = 0; i < projectImages.length; i++) {
     projectImages[i].style.filter = "sepia(1)"; 
+  }
+  for (let i = 0; i < projectParagraphs.length; i++) {
+    projectLabels[i].style.color = "darkgray";  
   }
   for (let i = 0; i < projectLabels.length; i++) {
     projectLabels[i].style.color = "gray";  
@@ -110,65 +129,89 @@ document.body.addEventListener("change", function (event) {
     resetAllProjects();
     scrollTopScreen();
     switch (event.target.id) {
-        case "RadioButton1-input":
+        case "busybees-readme-input":
           busybeesDiv.style.order = -1;
+          busybeesParagraph.style.color = "palegoldenrod";
           busybeesImage.style.filter = "none";
           busybeesReadMeLabel.style.color = "palegoldenrod";
           busybeesReadMeContent.style.display = "block";
           break;
-        case "RadioButton2-input":
+        case "busybees-designdoc-input":
           busybeesDiv.style.order = -1;
+          busybeesParagraph.style.color = "palegoldenrod";
           busybeesImage.style.filter = "none";
           busybeesDesignDocLabel.style.color = "palegoldenrod";
           busybeesDesignDocContent.style.display = "block";
           break;
-        case "RadioButton3-input":
+        case "busybees-code-input":
           busybeesDiv.style.order = -1;
+          busybeesParagraph.style.color = "palegoldenrod";
+          busybeesImage.style.filter = "none";
+          busybeesCodeLabel.style.color = "palegoldenrod";
+          busybeesCodeContent.style.display = "block";
+          break;
+        case "busybees-kanban-input":
+          busybeesDiv.style.order = -1;
+          busybeesParagraph.style.color = "palegoldenrod";
           busybeesImage.style.filter = "none";
           busybeesKanbanLabel.style.color = "palegoldenrod";
-          busybeesKanban.style.display = "block";
+          busybeesKanbanContent.style.display = "block";
           break;
-        case "RadioButton4-input":
+        case "busybees-demo-input":
           busybeesDiv.style.order = -1;
+          busybeesParagraph.style.color = "palegoldenrod";
           busybeesImage.style.filter = "none";
           busybeesDemoLabel.style.color = "palegoldenrod";
           busybeesDemoContent.style.display = "block";
           break;
-        case "RadioButton5-input":
+        case "busybees-report-input":
           busybeesDiv.style.order = -1;
+          busybeesParagraph.style.color = "palegoldenrod";
           busybeesImage.style.filter = "none";
-          busybeesProgressLabel.style.color = "palegoldenrod";
-          busybeesProgressContent.style.display = "block";
+          busybeesReportLabel.style.color = "palegoldenrod";
+          busybeesReportContent.style.display = "block";
           break;
-        case "RadioButton6-input":
+        case "journaltojson-readme-input":
           journaltojsonDiv.style.order = -1;
-          project2Image.style.filter = "none";
-          project2ReadMeLabel.style.color = "lightblue";
-          project2ReadMeContent.style.display = "block";
+          journaltojsonParagraph.style.color = "lightblue";
+          journaltojsonImage.style.filter = "none";
+          journaltojsonReadMeLabel.style.color = "lightblue";
+          journaltojsonReadMeContent.style.display = "block";
           break;
-        case "RadioButton7-input":
+        case "journaltojson-designdoc-input":
           journaltojsonDiv.style.order = -1;
-          project2Image.style.filter = "none";
-          project2DesignDocLabel.style.color = "lightblue";
-          project2DesignDocContent.style.display = "block";
+          journaltojsonParagraph.style.color = "lightblue";
+          journaltojsonImage.style.filter = "none";
+          journaltojsonDesignDocLabel.style.color = "lightblue";
+          journaltojsonDesignDocContent.style.display = "block";
           break;
-        case "RadioButton8-input":
+        case "journaltojson-code-input":
           journaltojsonDiv.style.order = -1;
-          project2Image.style.filter = "none";
-          project2KanbanLabel.style.color = "lightblue";
-          project2KanbanContent.style.display = "block";
+          journaltojsonParagraph.style.color = "lightblue";
+          journaltojsonImage.style.filter = "none";
+          journaltojsonCodeLabel.style.color = "lightblue";
+          journaltojsonCodeContent.style.display = "block";
           break;
-        case "RadioButton9-input":
+        case "journaltojson-kanban-input":
           journaltojsonDiv.style.order = -1;
-          project2Image.style.filter = "none";
-          project2DemoLabel.style.color = "lightblue";
-          project2DemoContent.style.display = "block";
+          journaltojsonParagraph.style.color = "lightblue";
+          journaltojsonImage.style.filter = "none";
+          journaltojsonKanbanLabel.style.color = "lightblue";
+          journaltojsonKanbanContent.style.display = "block";
           break;
-        case "RadioButton10-input":
+        case "journaltojson-demo-input":
           journaltojsonDiv.style.order = -1;
-          project2Image.style.filter = "none";
-          project2ProgressLabel.style.color = "lightblue";
-          project2ProgressContent.style.display = "block";
+          journaltojsonParagraph.style.color = "lightblue";
+          journaltojsonImage.style.filter = "none";
+          journaltojsonDemoLabel.style.color = "lightblue";
+          journaltojsonDemoContent.style.display = "block";
+          break;
+        case "journaltojson-report-input":
+          journaltojsonDiv.style.order = -1;
+          journaltojsonParagraph.style.color = "lightblue";
+          journaltojsonImage.style.filter = "none";
+          journaltojsonReportLabel.style.color = "lightblue";
+          journaltojsonReportContent.style.display = "block";
           break;
         case "RadioButton11-input":
           fiveminutefantasyDiv.style.order = -1;
